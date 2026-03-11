716ff7d3b7c91378d07887ebe72cd9aa3ff94963
Yehor Zhurenko <yehorzhurenko@gmail.com>
2026-03-11 12:51:26 +0200

    upd

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // важен для GitHub Pages: проект лежит в /Paletkin/, а не в корне домена
+ // base: '/',
   plugins: [react()],
 })
